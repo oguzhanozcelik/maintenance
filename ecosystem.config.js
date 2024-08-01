ab78066051f3ab3da140bd0d966a4c8ab02bb517
oguzhanozcelik <info@webmedya.co>
2024-08-01 18:48:01 -0400

    Port update v2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [
         {
-            name: 'laviniacraft',
+            name: 'mskbakim',
             script: 'npm',
             args: 'start',
             env: {
